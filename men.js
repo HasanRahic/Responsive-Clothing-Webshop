54df43d1f7bef9b716ac6d87f48436cda746f5e2
Hasan Rahic <haso.raha10@gmail.com>
2024-01-15 16:59:13 +0100

    first commit

--- men.js
+++ men.js
@@ -1,3 +1,11 @@
 
 
+var title = document.getElementsByClassName('shop-item-title').value;
+document.getElementsByClassName('shop-item-title')[0].innerText = title;
+var price = document.getElementsByClassName('price').value;
+document.getElementsByClassName('price')[0].innerText = price;
 
+document.getElementsByClassName('shop-item-button').addEventListener('click', function(){
+    localStorage.setItem("title", title)
+    localStorage.setItem("price", price);
+})
