9bfa9fbe671b51b54358930d68ace4861d59f0a8
Hasan Rahic <haso.raha10@gmail.com>
2024-01-21 22:14:51 +0100

    first commit

--- men.js
+++ men.js
@@ -4,8 +4,8 @@ var title = document.getElementsByClassName('shop-item-title')[0].innerText
 var price = document.getElementsByClassName('price')[0].innerText
 var img = document.getElementsByClassName('shop-item-img')[0].src
 
-document.addEventListener('click', () =>{
-    localStorage.setItem('title', title)
-    localStorage.setItem('price', price)
-    localStorage.setItem('image', img)
+document.addEventListener("click", () =>{
+    localStorage.setItem("title", title)
+    localStorage.setItem("price", price)
+    localStorage.setItem("image", img)
 })
