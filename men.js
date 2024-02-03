af743a4ab253fa2e68e087721dc4fc53e4359a34
Hasan Rahic <haso.raha10@gmail.com>
2024-02-03 01:58:58 +0100

    first commit

--- men.js
+++ men.js
@@ -3,107 +3,102 @@ if(document.readyState == 'loading'){
 } else{
     ready()
 }
+let cart = document.querySelectorAll('.shop-item-button')
+
+let products = [
+    {
+        title : 'Brown Coat',
+        tag: 'browncoat',
+        price : 249.99,
+        inCart: 0
+    },
+    {
+        title : 'Red Shirt with Long Sleeves',
+        tag: 'redshirtlongsleeves',
+        price : 59.99,
+        inCart: 0
+    },
+    {
+        title : 'White Hoodie',
+        tag: 'whitehoodie',
+        price : 79.99,
+        inCart: 0
+    },
+    {
+        title : 'Denim Jacket',
+        tag: 'denimjacket',
+        price : 129.99,
+        inCart: 0
+    },
+    {
+        title : 'Red Zip Up Sweatshirt',
+        tag: 'redsweatshirt',
+        price : 59.99,
+        inCart: 0
+    },
+    {
+        title : 'Gray Hoodie',
+        tag: 'greyhoodie',
+        price : 79.99,
+        inCart: 0
+    },
+    {
+        title : 'Black Hoodie',
+        tag: 'blackhoodie',
+        price : 79.99,
+        inCart: 0
+    },
+    {
+        title : 'Dark Blue Jeans',
+        tag: 'darkbluejeans',
+        price : 119.99,
+        inCart: 0
+    },
+    {
+        title : 'Yellow Shorts',
+        tag: 'yellowshorts',
+        price : 69.99,
+        inCart: 0
+    },
+    {
+        title : 'Bleu de Chanel Eau de Parfeum',
+        tag: 'bleuchanelparfeum',
+        price : 169.99,
+        inCart: 0
+    },
+    {
+        title : 'Tom Ford Ombré Leather',
+        tag: 'tomfordparfeum',
+        price : 200.00,
+        inCart: 0
+    },
+    {
+        title : 'Fogg Discover',
+        tag: 'foggdiscover',
+        price : 120.00,
+        inCart: 0
+    }
+]
+
+
+for (let i = 0; i < cart.length; i++){
+    cart[i].addEventListener('click', ()=>{
+        cartNumbers();
+    })
+}
+
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+    }else{
+        localStorage.setItem('cartNumbers', 1)
+    }
+}
 
 function ready(){
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    console.log(removeCartItemButtons)
-    for(var i = 0; i < removeCartItemButtons.length; i++){
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for(var i = 0; i < quantityInputs.length; i++){
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
-
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for(var i = 0; i < addToCartButtons.length; i++){
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
-    }
-
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
-}
-
-function purchaseClicked(){
-    alert('Thank you for your purchase')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while(cartItems.hasChildNodes()){
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()
-}
-
-
-function updateCartTotal(){
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for(var i = 0; i < cartRows.length; i++){
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-    }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
-}
-
-function removeCartItem(event){
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
-}
-
-function quantityChanged(event){
-    var input = event.target
-    if(isNaN(input.value) || input.value < 1){
-        input.value = 1
-    }
-    updateCartTotal()
-}
-
-function addToCartClicked(event){
-    var button = event.target
-    var shopItem = button.parentElement
-    var ShopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('price')[0].innerText
-    var imageSrc = ShopItem.getElementsByClassName('shop-item-img')[0].src
-    console.log(title, price, imageSrc)
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
-}
-
-function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName("cart-item-title")
-    for(var i = 0; i < cartItemNames.length; i++){
-        if(cartItemNames[i].innerText == title){
-            alert('This item is already added to the cart')
-            return
-        }
-    }
-    var cartRowContents = `
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
 
