8904e901be72ecd72cc8aa38a2b498bc93183601
Hasan Rahic <haso.raha10@gmail.com>
2024-02-05 13:20:36 +0100

    first commit

--- men.js
+++ men.js
@@ -1,4 +1,10 @@
 let cart = document.querySelectorAll('.shop-item-button')
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready();
+}
+function ready(){}
 
 let products = [
     {
@@ -15,7 +21,7 @@ let products = [
     },
     {
         title: 'White Hoodie',
-        tag: 'pexels-photo-1671399.webp',
+        tag: 'pexels-photo-8217419',
         price: 79.99,
         inCart: 0
     },
@@ -33,7 +39,7 @@ let products = [
     },
     {
         title: 'Gray Hoodie',
-        tag: 'pexels-photo-6311719',
+        tag: 'pexels-photo-6311603',
         price: 79.99,
         inCart: 0
     },
@@ -74,6 +80,67 @@ let products = [
         inCart: 0
     }
 ];
+
+
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for(var i = 0; i < removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', removeCartItem)
+}
+var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+for(var i = 0; i < quantityInputs.length; i++){
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged)
+}
+displayCart();
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+
+
+function purchaseClicked(){
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while(cartItems.hasChildNodes()){
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    localStorage.removeItem("cartNumbers");
+    localStorage.removeItem("productsInCart");
+    updateCartTotal();
+
+}
+
+
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for(var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
+function removeCartItem(event){
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal()
+}
+
+function quantityChanged(event){
+    var input = event.target
+    if(isNaN(input.value) || input.value < 1){
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+
 
 
 for (let i = 0; i < cart.length; i++){
@@ -121,20 +188,21 @@ function displayCart(){
         productContainer.innerHTML = '';
         Object.values(cartItems).map(el =>{
             productContainer.innerHTML +=`
-            <div class="cart-item cart-column">
-                <img class="cart-item-image" src="photos/${el.tag}.jpeg" width="100" height="100">
-                <span class="cart-item-title">${el.title}</span>
-            </div>
-            <span class="cart-price cart-column">${el.price}</span>
-            <div class="cart-quantity cart-column">
-                <input class="cart-quantity-input" type="number" value="1">
-                <button class="btn btn-danger" type="button">REMOVE</button>
-            </div>
+            <div class="cartitems">
+                <div class="cart-item cart-column">
+                    <img class="cart-item-image" src="photos/${el.tag}.jpeg" width="100" height="100">
+                    <span class="cart-item-title">${el.title}</span>
+                </div>
+                <span class="cart-price cart-column">$${el.price}</span>
+                <div class="cart-quantity cart-column">
+                    <input class="cart-quantity-input" type="number" value="1">
+                    <button class="btn btn-danger" type="button">REMOVE</button>
+                </div>
+            </div>    
             `
         });
     }
 }
-displayCart();
 
 
 
