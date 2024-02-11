c2054aace5b67ae32c4cda45afdcedba733b7165
Hasan Rahic <haso.raha10@gmail.com>
2024-02-11 01:59:17 +0100

    first commit

--- cart.js
+++ cart.js
@@ -1,28 +1,104 @@
+let cart = document.querySelectorAll('.shop-item-button')
 if(document.readyState == 'loading'){
     document.addEventListener('DOMContentLoaded', ready)
-} else{
-    ready()
+}else{
+    ready();
 }
+function ready(){}
 
-function ready(){
+let products = [
+    {
+        title: 'Brown Coat',
+        tag: 'pexels-photo-1183266',
+        price: 249.99,
+        inCart: 0
+    },
+    {
+        title: 'Red Shirt with Long Sleeves',
+        tag: 'pexels-photo-2095582',
+        price: 59.99,
+        inCart: 0
+    },
+    {
+        title: 'White Hoodie',
+        tag: 'pexels-photo-8217419',
+        price: 79.99,
+        inCart: 0
+    },
+    {
+        title: 'Denim Jacket',
+        tag: 'pexels-photo-2774163',
+        price: 129.99,
+        inCart: 0
+    },
+    {
+        title: 'Red Zip Up Sweatshirt',
+        tag: 'pexels-photo-1706912',
+        price: 59.99,
+        inCart: 0
+    },
+    {
+        title: 'Gray Hoodie',
+        tag: 'pexels-photo-6311603',
+        price: 79.99,
+        inCart: 0
+    },
+    {
+        title: 'Black Hoodie',
+        tag: 'pexels-photo-6311560',
+        price: 79.99,
+        inCart: 0
+    },
+    {
+        title: 'Dark Blue Jeans',
+        tag: 'pexels-photo-7228516',
+        price: 119.99,
+        inCart: 0
+    },
+    {
+        title: 'Yellow Shorts',
+        tag: 'pexels-photo-7228462',
+        price: 69.99,
+        inCart: 0
+    },
+    {
+        title: 'Bleu de Chanel Eau de Parfeum',
+        tag: 'pexels-photo-9202894',
+        price: 169.99,
+        inCart: 0
+    },
+    {
+        title: 'Tom Ford Ombré Leather',
+        tag: 'pexels-photo-7487834',
+        price: 200.00,
+        inCart: 0
+    },
+    {
+        title: 'Fogg Discover',
+        tag: 'pexels-photo-11265021',
+        price: 120.00,
+        inCart: 0
+    }
+];
+
+let cartItemsInCart = localStorage.getItem("productsInCart");
+if(cartItemsInCart){
     displayCart();
-
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    console.log(removeCartItemButtons)
-    for(var i = 0; i < removeCartItemButtons.length; i++){
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
-
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for(var i = 0; i < quantityInputs.length; i++){
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
-
-
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+    updateCartTotal();
 }
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for(var i = 0; i < removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', removeCartItem)
+}
+var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+for(var i = 0; i < quantityInputs.length; i++){
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged)
+}
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+
 
 function purchaseClicked(){
     alert('Thank you for your purchase')
@@ -30,29 +106,18 @@ function purchaseClicked(){
     while(cartItems.hasChildNodes()){
         cartItems.removeChild(cartItems.firstChild)
     }
-    updateCartTotal()
+    localStorage.removeItem("cartNumbers");
+    localStorage.removeItem("productsInCart");
+    updateCartTotal();
+
 }
 
 
-function updateCartTotal(){
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for(var i = 0; i < cartRows.length; i++){
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-    }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
-}
+
 
 function removeCartItem(event){
     var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
+    buttonClicked.parentElement.parentElement.remove();
     updateCartTotal()
 }
 
@@ -66,34 +131,85 @@ function quantityChanged(event){
 
 
 
-function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName("cart-item-title")
-    for(var i = 0; i < cartItemNames.length; i++){
-        if(cartItemNames[i].innerText == title){
-            alert('This item is already added to the cart')
-            return
+for (let i = 0; i < cart.length; i++){
+    cart[i].addEventListener('click', ()=>{
+        cartNumbers(products[i]);
+    })
+}
+
+function cartNumbers(product){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+    }else{
+        localStorage.setItem('cartNumbers', 1)
+    }
+    setItems(product);
+    updateCartTotal();
+}
+
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag]:product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else{
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]:product
         }
     }
-    var cartRowContents = `
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    updateCartTotal();
 }
-
 function displayCart(){
-    let cartItem = localStorage.getItem("")
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".cart-items");
+    if(cartItems && productContainer){
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(el =>{
+            productContainer.innerHTML +=`
+            <div class="cartitems">
+                <div class="cart-item cart-column">
+                    <img class="cart-item-image" src="photos/${el.tag}.jpeg" width="100" height="100">
+                    <span class="cart-item-title">${el.title}</span>
+                </div>
+                <span class="cart-price">$${el.price}</span>
+                <div class="cart-quantity cart-column">
+                    <input class="cart-quantity-input" type="number" value="1">
+                    <button class="btn btn-danger" type="button">REMOVE</button>
+                </div>
+            </div>
+            `
+        });
+    }
 }
 
+
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cartitems')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
