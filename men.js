9d4f7dec1ee817dc0d50d85ce9c7adbb628f8f89
Hasan Rahic <haso.raha10@gmail.com>
2024-02-15 10:53:10 +0100

    first commit

--- men.js
+++ men.js
@@ -101,7 +101,6 @@ document.getElementsByClassName('btn-purchase')[0].addEventListener('click', pur
 
 
 function purchaseClicked(){
-    alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while(cartItems.hasChildNodes()){
         cartItems.removeChild(cartItems.firstChild)
